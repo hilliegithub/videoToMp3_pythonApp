79e9650af433c48cea96e2f044326d7ccfdb7b64
Hylton <goregehylton@yahoo.com>
2023-12-24 14:52:41 -0600

    Created the infastructure code for auth service.

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -53,7 +53,7 @@ def createJWT(username, secret, authz):
     )
 
 
-@server.route("/validate", method=["POST"])
+@server.route("/validate", methods=["POST"])
 def validate():
     encoded_jwt = request.headers["Authorization"]
 
