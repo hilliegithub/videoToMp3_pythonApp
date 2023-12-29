be1d3e667264679952931bc54c92eb7f27e9dbc1
Hylton <goregehylton@yahoo.com>
2023-12-28 18:40:48 -0600

    Finished app. 😀

--- python/src/notification/send/email.py
+++ python/src/notification/send/email.py
@@ -1,4 +1,4 @@
-import smtplib, os, logging, json
+import smtplib, os, logging, json, ssl
 from email.message import EmailMessage
 
 logging.basicConfig(level=logging.DEBUG)
@@ -17,11 +17,18 @@ def notification(message):
         msg["From"] = sender_address
         msg["To"] = receiver_address
 
-        session = smtplib.SMTP("smtp.gmail.com", 587)
-        session.starttls()
-        session.login(sender_address, sender_password)
-        session.send_message(msg, sender_address, receiver_address)
-        session.quit()
+        #session = smtplib.SMTP("smtp.gmail.com", 587)
+        #session.starttls()
+        #session.login(sender_address, sender_password)
+        #session.send_message(msg, sender_address, receiver_address)
+        #session.quit()
+
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(sender_address, sender_password)
+            smtp.sendmail(sender_address,receiver_address, msg.as_string())
+
         logging.debug("Email sent!")
 
     except Exception as err:
